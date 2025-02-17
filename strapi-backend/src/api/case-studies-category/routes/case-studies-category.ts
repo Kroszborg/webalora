4b68e58ae3953c6b316eed66787781a148db418d
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-02-18 00:49:29 +0530

    strapi setup done

---
+++ strapi-backend/src/api/case-studies-category/routes/case-studies-category.ts
@@ -0,0 +1,7 @@
+/**
+ * case-studies-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::case-studies-category.case-studies-category');
---
+++ strapi-backend/src/api/case-studies-category/services/case-studies-category.ts
@@ -0,0 +1,7 @@
+/**
+ * case-studies-category service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::case-studies-category.case-studies-category');
